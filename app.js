dabeb846d962be253d042e8212a19a908673ed15
Kyle Devine <devinenoise@gmail.com>
2021-03-02 14:07:57 -0800

    working timer

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const button = document.querySelectorAll('[data-time]');
+
 function timer(seconds) {
-  
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check to stop the timer
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    //display it
+    displayTimeLeft(secondsLeft);
+  }, 1000);
 }
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60); // rounds the minutes to whole numbers
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${
+    remainderSeconds < 10 ? '0' : ''
+  }${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${
+    minutes < 10 ? '0' : ''
+  }${minutes}`; // makes 12 hour time
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time); // converts to real number
+  timer(seconds);
+}
+
+button.forEach(button => button.addEventListener('click', startTimer));
